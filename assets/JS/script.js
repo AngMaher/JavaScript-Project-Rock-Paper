930546db675b8b3d14f6629523f8ff34478e3c66
AngMaher <angelamaher@mail.com>
2022-10-26 12:59:28 +0000

    set up game area and add styling

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+let gameArea = document.getElementById('game-area');
+let heading = document.createElement("h2");
+heading.innerHTML = "Please click your choice below:";
+gameArea.append(heading);
+
+let options = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+
+
+for (let i = 0; i < options.length; i++) {
+    let button = document.createElement('button');
+    button.innerHTML = options[i];
+    button.addEventListener('click', makeChoice);
+    gameArea.append(button);
+}
+
+function makeChoice() {
+    console.log("hello");
+}
