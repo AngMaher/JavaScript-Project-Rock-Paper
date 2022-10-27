36e952dbf0123197f6a4e531adbbc6fd05031346
AngMaher <angelamaher@mail.com>
2022-10-27 18:37:48 +0000

    add js for adding images and connect to button choices

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,41 @@
-let gameArea = document.getElementById('game-area');
-let heading = document.createElement("h2");
-heading.innerHTML = "Please click your choice below:";
-gameArea.append(heading);
+let gameArea = document.getElementById('game-section');
+let buttons = document.getElementsByClassName('btn-choice');
+const computerImage = document.getElementById("computer-image")
+const playerImage = document.getElementById("player-image");
+let resultDisplay = document.getElementById('result-display');
 
-let options = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+let options = ["rock", "paper", "scissors", "lizard", "spock"];
 
-
-
-for (let i = 0; i < options.length; i++) {
-    let button = document.createElement('button');
-    button.innerHTML = options[i];
-    button.addEventListener('click', makeChoice);
-    gameArea.append(button);
+for (let button of buttons){
+    button.addEventListener('click', function (){
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
 }
 
-function makeChoice() {
-    console.log("hello");
+function playGame(playerChoice){
+
+    playerImage.src=`assets/images/${options[playerChoice]}.jpg`;
+    playerImage.alt=options[playerChoice];
+
+    let computerChoice = Math.floor(Math.random()*5);
+
+    computerImage.src=`assets/images/${options[computerChoice]}.jpg`;
+    computerImage.alt=options[computerChoice];
+    let player=options[playerChoice];
+    let computer=options[computerChoice];
+
+    let result = checkWinner(player, computer);
+
+    changeScores(result);
+}
+
+function checkWinner(player,computer) {
+    result = player+computer;
+
+    
+} 
+
+function changeScores(){
+    console.log("changeScores function called");
 }
